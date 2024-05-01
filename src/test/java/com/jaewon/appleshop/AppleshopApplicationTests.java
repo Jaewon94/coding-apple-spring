c873115dd954d05218331ebf967e167baf47def9
Jaewon94 <wodnjs3580@gmail.com>
2024-05-01 09:34:23 +0900

    init

---
+++ src/test/java/com/jaewon/appleshop/AppleshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaewon.appleshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
